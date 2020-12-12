620e52721ce5345234b0af3fe150e72378939d90
Anton Borkivskyi <anton.borkivskyi@gmail.com>
2020-12-12 20:32:31 +0200

    Clean redundant code

--- flask_http/views.py
+++ flask_http/views.py
@@ -1,22 +1,14 @@
-import aiohttp
 import requests
 from flask import request, jsonify
 from flask.views import MethodView
 from requests.exceptions import ReadTimeout
-import asyncio
 
 lst = []
 secondaries = ["secondary-1", "secondary-2"]
-secondaries = ["127.0.0.1"]
 PORT = 9001
 
 
 class MessageView(MethodView):
-
-    async def send_req(self, domain, message):
-        r = await aiohttp.post(f'http://{domain}:{PORT}/secondary', data={"rep_message": message})
-        # r = await requests.post(f'http://{domain}:{PORT}/secondary', json={'rep_message': message})
-        return r.status_code == 201
 
     def get(self):
         return jsonify({"messages": lst})
