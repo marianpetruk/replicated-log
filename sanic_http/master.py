e28e42c2232bd3a2921036ec07c0d7293392cebb
Anton Borkivskyi <anton.borkivskyi@gmail.com>
2020-12-30 23:21:05 +0200

    Retry. Deduplication. Total order

--- sanic_http/master.py
+++ sanic_http/master.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from requests.exceptions import ReadTimeout, ConnectionError
 from sanic import Sanic
@@ -5,39 +7,80 @@ from sanic.response import json
 
 from sync_tools import CountDownLatch
 
-from gevent import monkey, sleep
-
-monkey.patch_all()
-
 import threading
 
-lst = []
+dct = {}
+id_ = 0
 secondary_domain = "127.0.0.1"
 secondaries = ["secondary-1", "secondary-2"]
 PORT = 9001
 ports = [9001, 9002]
 app = Sanic("Master node")
 
-#TODO rewrite to create dict dynamically
+# TODO rewrite to create dict dynamically
 statuses = ["Healthy", "Suspected", "Unhealthy"]
 secondaries_status = {9001: "Healthy", 9002: "Healthy"}
+
+secondaries_unsent = {9001: [], 9002: []}
 
 mode = "w"
 
 
 @app.route("/")
 async def get(request):
-    return json({"messages": lst})
+    return json({"messages": list(dict(sorted(dct.items(), key=lambda item: item[0])).values())})
 
 
 def post_one(cdl, port, message):
     try:
         r = requests.post(f'http://{secondary_domain}:{port}/', json={'rep_message': message}, timeout=5)
     except (ReadTimeout, ConnectionError):
+        if secondaries_status[port] != "Unhealthy":
+            secondaries_status[port] = statuses[statuses.index(secondaries_status[port]) + 1]
+        secondaries_unsent[port].append(message)
+        # TODO check timeouted for master POST
         return False
     if r.status_code == 201:
         cdl.count_down()
+    else:
+        if secondaries_status[port] != "Unhealthy":
+            secondaries_status[port] = statuses[statuses.index(secondaries_status[port]) + 1]
+        secondaries_unsent[port].append(message)
     return r.status_code == 201
+
+
+def retry_request(port, message):
+    try:
+        r = requests.post(f'http://{secondary_domain}:{port}/', json={'rep_message': message}, timeout=5)
+    except (ReadTimeout, ConnectionError):
+        if secondaries_status[port] != "Unhealthy":
+            secondaries_status[port] = statuses[statuses.index(secondaries_status[port]) + 1]
+        return False
+    if r.status_code == 201:
+        secondaries_unsent[port].remove(message)
+    else:
+        if secondaries_status[port] != "Unhealthy":
+            secondaries_status[port] = statuses[statuses.index(secondaries_status[port]) + 1]
+    return True
+
+
+def retry(port):
+    for unsent_message in secondaries_unsent[port]:
+        thread = threading.Thread(target=retry_request, args=(port, unsent_message))
+        thread.daemon = True  # Daemonize thread
+        thread.start()  # Start the execution
+
+
+def monitor_secondaries():
+    while True:
+        prev_statuses = secondaries_status.copy()
+
+        for port in ports:
+            health_check_one(port)
+            if prev_statuses[port] != "Healthy" and secondaries_status[port] == "Healthy":
+                retry(port)
+
+        time.sleep(30)
 
 
 def health_check_one(port):
@@ -55,9 +98,11 @@ def health_check_one(port):
 
 @app.route('/', methods=['POST'])
 async def post(request):
-
+    global id_
     num_of_alive = list(secondaries_status.values()).count("Healthy")
-    if num_of_alive != len(secondaries):
+
+    # We assume quorum is when at most one of secondaries is not present
+    if len(secondaries) - num_of_alive > 1:
         mode = "ro"
     else:
         mode = "w"
@@ -65,19 +110,20 @@ async def post(request):
     if mode == "ro":
         return json({'description': 'Some of secondaries are not available'}, status=502)
 
-
-    #TODO return error if no message in request
+    # TODO return error if no message in request
     message = request.json.get("message")
     write_concern = request.json.get("w")
     write_concern = len(secondaries) + 1 if write_concern is None else write_concern
-    lst.append(message)
+    dct[id_] = message
 
     cdl = CountDownLatch(count=write_concern - 1)
 
     for port in ports:
-        thread = threading.Thread(target=post_one, args=(cdl, port, message))
+        thread = threading.Thread(target=post_one, args=(cdl, port, {id_: message}))
         thread.daemon = True  # Daemonize thread
         thread.start()  # Start the execution
+
+    id_ += 1
 
     if write_concern == 1:  # write concern 1 and therefore only master has to write the data
         return json({'description': 'Success'}, status=201)
@@ -89,21 +135,24 @@ async def post(request):
 
 @app.route('/health', methods=['GET'])
 async def healthcheck(request):
-    thread_lists = []
-    for port in ports:
-        thread = threading.Thread(target=health_check_one, args=(port,))
-        thread.daemon = False  # Daemonize thread
-        thread_lists.append(thread)
-        thread.start() # Start the execution
-
-    for thread in thread_lists:
-        thread.join()
+    # thread_lists = []
+    # for port in ports:
+    #     thread = threading.Thread(target=health_check_one, args=(port,))
+    #     thread.daemon = False  # Daemonize thread
+    #     thread_lists.append(thread)
+    #     thread.start() # Start the execution
+    #
+    # for thread in thread_lists:
+    #     thread.join()
 
     return json({"status": secondaries_status})
 
 
-
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9000, workers=2)
+    # TODO find out how to share state between workers
+
+    monitoring_thread = threading.Thread(target=monitor_secondaries)
+    monitoring_thread.daemon = True
+    monitoring_thread.start()
+
+    app.run(host="0.0.0.0", port=9000, workers=1)
--- sanic_http/secondary.py
+++ sanic_http/secondary.py
@@ -2,28 +2,55 @@ from sanic import Sanic
 from sanic.response import json
 import asyncio
 
-lst = []
+dct = {}
 app = Sanic("App Name")
+
+
+# k = 0 for test purposes
 
 
 @app.route("/", methods=['GET'])
 async def get(request):
-    return json({"messages": lst})
+    sorted_dct = dict(sorted(dct.items(), key=lambda item: item[0]))
+    if len(sorted_dct) == 0:
+        return json({"messages": []})
+
+    for i in range(len(sorted_dct)):
+        if i not in sorted_dct:
+            i -= 1
+            break
+
+    messages = list(sorted_dct.values())
+
+    return json({"messages": messages[:i + 1]})
 
 
 @app.route("/", methods=['POST'])
 async def post(request):
-    await asyncio.sleep(10)  # TODO: for testing sleepy secondary
+    # global k
+    # await asyncio.sleep(10)  # for testing sleepy secondary
     # time.sleep(25) # for testing sleepy secondary
+
     rep_message = request.json.get("rep_message")
-    lst.append(rep_message)
+    key_ = list(rep_message.keys())[0]
+
+    if int(key_) not in dct:
+        # Test total order and retries:
+        #
+        # if int(key_) == 3 and k == 0:
+        #     k += 1
+        #     dct.update({int(key_)+1: "Z"})
+        #
+        #     Simulate internal error:
+        #     return json({'description': 'Replication Success', 'status_code': 502}, status=502)  # TODO: fix double status code
+
+        dct.update({int(key_): rep_message[key_]})
     return json({'description': 'Replication Success', 'status_code': 201}, status=201)  # TODO: fix double status code
+
 
 @app.route('/health', methods=['GET'])
 async def healthcheck(request):
     return json({"status": "OK"})
-
-
 
 
 if __name__ == "__main__":
