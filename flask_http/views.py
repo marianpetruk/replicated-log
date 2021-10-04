e4f17cbe2ab1a9efea91dc89c315e1abce976551
Bohdan Borkivskyi <bohdan.borkivskyi@quintagroup.org>
2021-10-04 22:41:43 +0300

    Add debug mode

--- flask_http/views.py
+++ flask_http/views.py
@@ -13,19 +13,30 @@ class MessageView(MethodView):
     def get(self):
         return jsonify({"messages": lst})
 
-    def error_400(self):
+    @staticmethod
+    def error_400():
         return jsonify({'description': 'Failed'}), 400
 
     def post(self):
-        message = request.get_json().get("message")
+        params = request.get_json()
+        message = params.get("message")
+        timeout = int(params.get("timeout", 0))
+
         lst.append(message)
+
+        request_data = {
+            "json": {'rep_message': message}
+        }
+        if timeout:
+            request_data['timeout'] = timeout
 
         for domain in secondaries:
             try:
-                # r = requests.post(f'http://{domain}:{PORT}/secondary', json={'rep_message': message}, timeout=1)  # for testing sleepy secondary
-                r = requests.post(f'http://{domain}:{PORT}/secondary', json={'rep_message': message})
+                r = requests.post(f'http://{domain}:{PORT}/secondary', **request_data)
             except ReadTimeout:
                 return self.error_400()
+
             if r.status_code != 201:
                 return self.error_400()
+
         return jsonify({'description': 'Success'}), 201
